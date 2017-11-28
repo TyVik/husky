acfc82bcae27cb86489d8ea5fb82d2d4fe72d0ec
TyVik <tyvik8@gmail.com>
2017-11-28 20:31:16 +0300

    Add admin for Quiz app.

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,24 @@
+from adminsortable.admin import SortableTabularInline, SortableAdmin
 from django.contrib import admin
 
-# Register your models here.
+from quiz.models import Quiz, Question, Answer
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 0
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline]
+
+
+class QuestionInline(SortableTabularInline):
+    model = Question
+    extra = 0
+
+
+@admin.register(Quiz)
+class QuizAdmin(SortableAdmin):
+    inlines = [QuestionInline]
--- quiz/models.py
+++ quiz/models.py
@@ -1,17 +1,41 @@
+from adminsortable.models import SortableMixin
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
 
 class Quiz(models.Model):
     title = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name = 'Quiz'
+        verbose_name_plural = 'Quizzes'
 
-class Question(models.Model):
+    def __str__(self):
+        return self.title
+
+
+class Question(SortableMixin, models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-    text = models.TextField()
-    order = models.PositiveSmallIntegerField()
+    text = models.CharField(max_length=200)
+    order = models.PositiveSmallIntegerField(default=0, editable=False, db_index=True)
+
+    class Meta:
+        ordering = ('order',)
+
+    def __str__(self):
+        return self.text[:20]
 
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     text = models.CharField(max_length=150)
     is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text[:20]
+
+
+class QuizResult(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    # answers = JSONField()  # avoid direct references to Answers. It's allow to manipulate them.
