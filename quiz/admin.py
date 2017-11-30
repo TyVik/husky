70d8cbfbba5f671dc7b9f3a5641633fb3d313fdc
TyVik <tyvik8@gmail.com>
2017-11-30 20:53:32 +0300

    Add AnswerInline form validation.

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,11 +1,23 @@
 from adminsortable.admin import SortableTabularInline, SortableAdmin
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
 from quiz.models import Quiz, Question, Answer, QuizResult
 
 
+class AnswerFormSet(BaseInlineFormSet):
+    def clean(self):
+        super(AnswerFormSet, self).clean()
+        for form in self.forms:
+            if form.cleaned_data.get('is_correct', False):
+                return
+        raise ValidationError('Need at least one correct answer.')
+
+
 class AnswerInline(admin.TabularInline):
     model = Answer
+    formset = AnswerFormSet
     extra = 0
 
 
